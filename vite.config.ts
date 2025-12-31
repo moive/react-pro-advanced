920c72db465bd72264f32524f1007fa9d9dc1779
Moises Velasquez <moises007@gmail.com>
2025-12-31 16:42:53 -0500

    fixes config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { spawn } from "child_process";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "typescript-check",
+      apply: "serve",
+      configResolved() {
+        // Ejecuta TypeScript check al iniciar el servidor
+        const tsc = spawn("node", [
+          resolve("node_modules/typescript/bin/tsc"),
+          "--noEmit",
+        ]);
+        tsc.stderr?.pipe(process.stderr);
+        tsc.stdout?.pipe(process.stdout);
+      },
+      handleHotUpdate() {
+        // Ejecuta TypeScript check en cada cambio
+        const tsc = spawn("node", [
+          resolve("node_modules/typescript/bin/tsc"),
+          "--noEmit",
+        ]);
+        tsc.stderr?.pipe(process.stderr);
+        tsc.stdout?.pipe(process.stdout);
+      },
+    },
+  ],
   css: {
     modules: {
       scopeBehaviour: "local",
