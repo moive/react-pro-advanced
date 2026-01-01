a6a9f2fda87332733ad618f7c8f9b55198d77014
Moises Velasquez <moises007@gmail.com>
2025-12-31 23:12:50 -0500

    first refactoring

--- src/patterns/pages/ShoppingPage.tsx
+++ src/patterns/pages/ShoppingPage.tsx
@@ -25,14 +25,30 @@ export const ShoppingPage = () => {
   const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
 
   const onProductCountChange = ({ count, product }: { count: number; product: Product }) => {
-    // console.log("onProductCountChange", count, product);
+    console.log({ count });
     setShoppingCart((oldValue) => {
-      if (count === 0) {
-        const { [product.id]: toDelete, ...rest } = oldValue;
-        return rest;
+      const productInCart: ProductInCart = oldValue[product.id] || { ...product, count: 0 };
+
+      if (Math.max(productInCart.count + count, 0) > 0) {
+        return {
+          ...oldValue,
+          [product.id]: {
+            ...productInCart,
+            count: productInCart.count + count,
+          },
+        };
       }
-      return { ...oldValue, [product.id]: { ...product, count } };
+
+      const { [product.id]: toDelete, ...rest } = oldValue;
+      return rest;
+
+      // if (count === 0) {
+      //   const { [product.id]: toDelete, ...rest } = oldValue;
+      //   return rest;
+      // }
+      // return { ...oldValue, [product.id]: { ...product, count } };
     });
+    console.log(shoppingCart);
   };
   return (
     <div>
