7617d449c28ff424c73fe92ad06a08dcb3daa6ca
Moises Velasquez <moises007@gmail.com>
2025-12-31 23:50:23 -0500

    refactoring alternative

--- src/patterns/hooks/useProduct.ts
+++ src/patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { onChangeArgs, Product } from "../interfaces/product.interface";
 
 interface useProductArgs {
@@ -10,30 +10,15 @@ interface useProductArgs {
 export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => {
   const [counter, setCounter] = useState(value);
 
-  const isControlled = useRef(!!onChange);
-
   const increaseBy = (value: number) => {
-    if (isControlled.current) {
-      return onChange!({ count: value, product });
-    }
-
     const newValue = Math.max(counter + value, 0);
     setCounter(newValue);
     onChange && onChange({ count: newValue, product });
-    // setCounter((prev) => Math.max(prev + value, 0));
   };
 
   useEffect(() => {
     setCounter(value);
   }, [value]);
-
-  // useEffect(() => {
-  //   setCounter(value);
-  // }, [value]);
-
-  // useEffect(() => {
-  //   onChange && onChange({ count: counter, product });
-  // }, [counter, product]);
 
   return {
     counter,
--- src/patterns/hooks/useShoppingCart.ts
+++ src/patterns/hooks/useShoppingCart.ts
@@ -7,26 +7,11 @@ export const useShoppingCart = () => {
   const onProductCountChange = ({ count, product }: { count: number; product: Product }) => {
     // console.log({ count });
     setShoppingCart((oldValue) => {
-      const productInCart: ProductInCart = oldValue[product.id] || { ...product, count: 0 };
-
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        return {
-          ...oldValue,
-          [product.id]: {
-            ...productInCart,
-            count: productInCart.count + count,
-          },
-        };
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldValue;
+        return rest;
       }
-
-      const { [product.id]: toDelete, ...rest } = oldValue;
-      return rest;
-
-      // if (count === 0) {
-      //   const { [product.id]: toDelete, ...rest } = oldValue;
-      //   return rest;
-      // }
-      // return { ...oldValue, [product.id]: { ...product, count } };
+      return { ...oldValue, [product.id]: { ...product, count } };
     });
   };
   return {
