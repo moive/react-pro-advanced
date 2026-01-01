1c278e227a1abfaeec264a1871265f6be342fc7d
Moises Velasquez <moises007@gmail.com>
2026-01-01 00:14:07 -0500

    changes

--- src/patterns/pages/ShoppingPage.tsx
+++ src/patterns/pages/ShoppingPage.tsx
@@ -2,48 +2,22 @@ import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../comp
 import "../styles/custom-styles.css";
 
 import { products } from "../../data/product";
-import { useShoppingCart } from "../hooks/useShoppingCart";
+
+const product = products[0];
 
 export const ShoppingPage = () => {
-  const { shoppingCart, onProductCountChange } = useShoppingCart();
-
   return (
     <div>
       <h1>Shopping Store</h1>
       <hr />
       <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-        {products.map((product) => (
-          <ProductCard
-            product={product}
-            className="bg-dark"
-            key={product.id}
-            onChange={(event) => onProductCountChange(event)}
-            value={shoppingCart[product.id]?.count || 0}
-          >
-            <ProductImage img={product.img} className="custom-image" />
-            <ProductTitle title={"no problems"} className="text-white text-bold text-capitalize" />
-            <ProductButtons className="custom-buttons" />
-          </ProductCard>
-        ))}
+        <ProductCard product={product} className="bg-dark" key={product.id}>
+          <ProductImage img={product.img} className="custom-image" />
+          <ProductTitle title={"no problems"} className="text-white text-bold text-capitalize" />
+          <ProductButtons className="custom-buttons" />
+        </ProductCard>
       </div>
-      <div className="shopping-cart">
-        {Object.entries(shoppingCart).map(([index, product]) => (
-          <ProductCard
-            product={product}
-            className="bg-dark"
-            style={{ width: "100px" }}
-            key={index}
-            value={product.count}
-            onChange={onProductCountChange}
-          >
-            <ProductImage img={product.img} className="custom-image" />
-            <ProductButtons
-              className="custom-buttons"
-              style={{ display: "flex", justifyContent: "center" }}
-            />
-          </ProductCard>
-        ))}
-      </div>
+
       {/* <div>
         <code>{JSON.stringify(shoppingCart, null, 5)}</code>
       </div> */}
