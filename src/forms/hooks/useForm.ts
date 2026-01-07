bb12e2abfda039ee538b4040852f365f2379d6a0
Moises Velasquez <moises007@gmail.com>
2026-01-07 01:45:26 -0500

    changes useForm

--- src/forms/hooks/useForm.ts
+++ src/forms/hooks/useForm.ts
@@ -7,5 +7,13 @@ export const useForm = <T>(initialData: T) => {
     setFormData((prev) => ({ ...prev, [event.target.name]: event.target.value }));
   };
 
-  return { ...formData, formData, onChange };
+  const reset = () => setFormData({ ...initialData });
+
+  const isValidEmail = (email: string) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  };
+
+  return { ...formData, formData, onChange, reset, isValidEmail };
 };
--- src/forms/pages/RegisterPage.tsx
+++ src/forms/pages/RegisterPage.tsx
@@ -3,12 +3,13 @@ import "../styles/styles.css";
 import { useForm } from "../hooks/useForm";
 
 export const RegisterPage = () => {
-  const { formData, name, email, password, confirmPassword, onChange } = useForm({
-    name: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
+  const { formData, onChange, reset, name, email, password, confirmPassword, isValidEmail } =
+    useForm({
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
 
   const onSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -19,23 +20,53 @@ export const RegisterPage = () => {
     <div>
       <h1>RegisterPage</h1>
       <form noValidate onSubmit={(ev) => onSubmit(ev)}>
-        <input type="text" placeholder="Name" value={name} onChange={onChange} name="name" />
-        <input type="email" placeholder="Email" value={email} onChange={onChange} name="email" />
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={onChange}
+          name="name"
+          className={`${name.trim().length <= 0 && "has-error"}`}
+        />
+        {name.trim().length <= 0 && <span>This field is required *</span>}
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={onChange}
+          name="email"
+          className={`${!isValidEmail(email) && "has-error"}`}
+        />
+        {name.trim().length <= 0 && <span>Email is required *</span>}
+        {!isValidEmail(email) && <span>Email is invalid</span>}
         <input
           type="password"
           placeholder="Password"
           value={password}
           onChange={onChange}
           name="password"
+          className={`${password.trim().length <= 0 && "has-error"}`}
         />
+        {password.trim().length <= 0 && <span>This field is required *</span>}
+        {password.trim().length < 6 && password.trim().length > 0 && (
+          <span>Lenght min is 6 characters</span>
+        )}
         <input
           type="password"
           placeholder="Confirm Password"
           value={confirmPassword}
           onChange={onChange}
           name="confirmPassword"
+          className={`${confirmPassword.trim().length <= 0 && "has-error"}`}
         />
+        {confirmPassword.trim().length <= 0 && <span>This field is required *</span>}
+        {confirmPassword.trim().length > 0 && confirmPassword !== password && (
+          <span>Password does not match</span>
+        )}
         <button type="submit">Create</button>
+        <button type="reset" onClick={reset}>
+          Reset
+        </button>
       </form>
     </div>
   );
